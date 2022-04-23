fb9831ca023354b3f32df5b45358238a3fbd8828
Michael <G00411301@gmit.ie>
2022-04-23 13:09:41 +0100

    Initial notes/comments made to the Readme file

--- analysis.py
+++ analysis.py
@@ -1,3 +1,4 @@
 # analysis.py
 # This program will read inputs from a specified csv file and out put a summary of the information, generate a histogram and scateer gragh and save them within the folder
 # Author Michael Casey
+
